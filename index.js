43672e36fee4418b9c2bf420e00d513d1969fee3
pradana4648 <wakwaujaim@gmail.com>
2022-10-28 15:25:56 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const Hapi = require('@hapi/hapi')
+const Path = require('path')
+
+;(async function () {
+  const server = Hapi.server({
+    debug:
+      process.env.NODE_ENV == 'development'
+        ? {
+            log: ['*'],
+            request: ['*'],
+          }
+        : undefined,
+    port: 8000,
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, 'public'),
+      },
+    },
+  })
+
+  await server.register(require('@hapi/vision'))
+  await server.register(require('@hapi/inert'))
+
+  /*
+   * Views
+   *
+   * Register Views
+   */
+  server.views({
+    engines: { html: require('handlebars') },
+    relativeTo: __dirname,
+    path: 'public',
+  })
+
+  /**
+   *  Routes
+   *
+   *  Public Route
+   */
+  server.route([
+    {
+      method: ['GET'],
+      path: '/',
+      handler: (req, h) => {
+        return h.view('index').code(200)
+      },
+    },
+    {
+      method: ['GET'],
+      path: '/css/main.css',
+      handler: (req, h) => {
+        return h.file('css/main.css').code(200)
+      },
+    },
+    {
+      method: ['GET'],
+      path: '/about',
+      handler: (req, h) => {
+        return h.view('about').code(200)
+      },
+    },
+    {
+      method: ['*'],
+      path: '/{any*}',
+      handler: (req, h) => {
+        return h.view('404').code(200)
+      },
+    },
+  ])
+
+  await server.start()
+  console.log('Server running at:', server.info.uri)
+})()
